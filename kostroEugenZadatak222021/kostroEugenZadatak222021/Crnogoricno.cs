ebe67c69083e1ddd19c147f9328435b6ec175bd5
Eugen101 <55134543+EugenKostro@users.noreply.github.com>
2021-02-02 10:53:37 +0100

    Add files via upload

---
+++ kostroEugenZadatak222021/kostroEugenZadatak222021/Crnogoricno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kostroEugenZadatak222021
+{
+    class Crnogoricno : Stablo
+    {
+        public Crnogoricno(bool opadajuListovi)
+        {
+
+        }
+    }
+}
---
+++ kostroEugenZadatak222021/kostroEugenZadatak222021/Stablo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kostroEugenZadatak222021
+{
+    class Stablo : Biljka
+    {
+        bool opadajuListovi;
+        private bool OpadajuListovi()
+        {
+            return opadajuListovi;
+        }
+        public void stablo(bool opadajuListovi)
+        {
+            this.opadajuListovi = opadajuListovi;
+        }
+    }
+}
